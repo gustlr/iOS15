596f24d2410ac5923d3c60f3e99c1da3a7495126
gustlr <gustlr@gmail.com>
2022-03-23 12:32:05 -0400

    adding Row

--- Fructus/Fructus/App/ContentView.swift
+++ Fructus/Fructus/App/ContentView.swift
@@ -8,14 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    var fruit:[Fruit] = fruitsData
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            List{
+                ForEach(fruit.shuffled()){
+                    item in
+                    FruitRowView(fruit: item)
+                        .padding(.vertical, 4)
+                }
+            }
+            .navigationTitle("Fruits")
+        }
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(fruit:fruitsData)
     }
 }
