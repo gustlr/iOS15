cd3536a33fb2531ca3303e35d82923e24ddc43cd
gustlr <gustlr@gmail.com>
2022-03-28 16:28:01 -0400

    create DisclosureGroup

--- Fructus/Fructus/App/FruitDetailView.swift
+++ Fructus/Fructus/App/FruitDetailView.swift
@@ -29,7 +29,7 @@ struct FruitDetailView: View {
                             .multilineTextAlignment(.leading)
                         
                         // NUTRIENTS
-                        
+                        FruitNutrientsView(fruit: fruit)
                         // SUBHEADLINE
                         Text("Learn more about \(fruit.title)".uppercased())
                             .fontWeight(.bold)
---
+++ Fructus/Fructus/View/FruitNutrientsView.swift
@@ -0,0 +1,44 @@
+//
+//  FruitNutrientsView.swift
+//  Fructus
+//
+//  Created by Hyun Shik Chung on 3/28/22.
+//
+
+import SwiftUI
+
+struct FruitNutrientsView: View {
+    var fruit: Fruit
+    let nutrients: [String] = ["Energey","Suger", "Fat", "Protein", "Vitamins", "Minerals"]
+    var body: some View {
+        GroupBox{
+            DisclosureGroup("Nuritional value per 100g"){
+                ForEach(0..<nutrients.count, id: \.self) { item in
+                    Divider().padding(.vertical, 2)
+                    HStack {
+                        Group {
+                            Image(systemName: "info.circle")
+                            Text(nutrients[item])
+                        }
+                        .foregroundColor(fruit.gradientColors[1])
+                        .font(Font.system(.body).bold())
+                            Spacer(minLength: 25)
+                        Text(fruit.nutrition[item])
+                            .multilineTextAlignment(.trailing)
+                    }
+                }
+            }
+            
+        }
+       
+    }
+}
+
+struct FruitNutrientsView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitNutrientsView(fruit: fruitsData[0])
+            .preferredColorScheme(.dark)
+            .previewLayout(.fixed(width: 375, height: 480))
+            .padding()
+    }
+}
