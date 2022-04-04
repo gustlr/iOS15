3cf5f56d50649dab58edcbc2751de2fee2d7e648
gustlr <gustlr@gmail.com>
2022-04-04 16:36:25 -0400

    Add Africa Project

---
+++ Africa/Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Hyun Shik Chung on 4/4/22.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
