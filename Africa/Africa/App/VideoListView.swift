7c9a31a5d7ce364f29a29a400c308bd375d3190c
gustlr <gustlr@gmail.com>
2022-04-04 22:16:16 -0400

    Ready for building app!!

---
+++ Africa/Africa/App/VideoListView.swift
@@ -0,0 +1,20 @@
+//
+//  VideoListView.swift
+//  Africa
+//
+//  Created by Hyun Shik Chung on 4/4/22.
+//
+
+import SwiftUI
+
+struct VideoListView: View {
+    var body: some View {
+        Text("Videos")
+    }
+}
+
+struct VideoListView_Previews: PreviewProvider {
+    static var previews: some View {
+        VideoListView()
+    }
+}
--- Africa/AfricaUITests/AfricaUITests.swift
+++ Africa/AfricaUITests/AfricaUITests.swift
@@ -8,7 +8,6 @@
 import XCTest
 
 class AfricaUITests: XCTestCase {
-
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
@@ -22,14 +21,7 @@ class AfricaUITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
 
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
 
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
@@ -39,4 +31,15 @@ class AfricaUITests: XCTestCase {
             }
         }
     }
+    
+    func mainViewTest() throws{
+        let app = XCUIApplication()
+        app.launch()
+        let listOfTabsButtons = ["Browse","Watch", "Locations", "Gallery"]
+        listOfTabsButtons.forEach { button in
+            XCTAssertTrue(app.buttons[button].exists)
+        }
+        
+    }
+    
 }
